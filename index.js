9a316b4ec0a69a0fc87ffbbb2f2e2ca771e0f692
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-10-04 20:51:32 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+//function to divide numbers
+function divide(){
+    return 2000/100;
+}
+
+//arrow function to square
+const square = (para) => para*para;
+
+//arrow function called add
+const add= (num1, num2) => num1+num2;
